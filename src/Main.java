d2a47b5b40de38763faa4b7a2e0cef3d7a6609e0
Satyam Singh <satyam.singh@peoplestrong.com>
2024-09-03 02:50:59 +0530

    TestCase Setup

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,96 @@
-import java.lang.reflect.InvocationTargetException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
         Method method = solution.getClass().getDeclaredMethods()[0];
-        System.out.println(method.invoke(solution,
-                5, parse2D("[[4,1,-1],[2,0,-1],[0,3,-1],[4,3,-1]]"), 0, 1, 5
-        ));
-    }
-
-    public static int[][] parse2D(String s) {
-        List<int[]> list = new ArrayList<>();
-        for (String pair : s.substring(2, s.length() - 2).split("],\\[")) {
-            String[] nums = pair.split(",");
-            int[] arr = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                arr[i] = Integer.parseInt(nums[i].trim());
+        try (BufferedReader br = new BufferedReader(new FileReader(".\\src\\TestCase.txt"))) {
+            while (br.ready()) {
+                Object[] params = new Object[method.getParameterCount()];
+                for (int i = 0; i < params.length; i++) {
+                    Class<?> clazz = method.getParameterTypes()[i];
+                    params[i] = TypeCaster.castTo(parse(br.readLine(), clazz), clazz);
+                }
+                System.out.println(method.invoke(solution, params));
             }
-            list.add(arr);
         }
-        return list.toArray(new int[list.size()][]);
     }
 
-    public static int[] parse1D(String s) {
-        List<Integer> list = new ArrayList<>();
-        for (String num : s.substring(1, s.length() - 1).split(",")) {
-            list.add(Integer.parseInt(num.trim()));
+    private static Object parse(String s, Class<?> clazz) {
+        if (clazz.isArray()) {
+            List<Object> list = new ArrayList<>();
+            for (String pair : s.substring(1, s.length() - 1).split(",")) {
+                list.add(parse(pair, clazz.getComponentType()));
+            }
+            return list.toArray();
+//            return Arrays.stream(s.substring(1, s.length() - 1).split(",")).map(x -> parse(x, clazz.getComponentType())).toArray();
+        } else {
+            if (Objects.equals(clazz, int.class)) {
+                return Integer.parseInt(s);
+            } else if (Objects.equals(clazz, long.class)) {
+                return Long.parseLong(s);
+            } else if (Objects.equals(clazz, char.class)) {
+                return s.charAt(0);
+            } else if (Objects.equals(clazz, float.class)) {
+                return Float.parseFloat(s);
+            } else if (Objects.equals(clazz, boolean.class)) {
+                return Boolean.parseBoolean(s);
+            } else if (Objects.equals(clazz, double.class)) {
+                return Double.parseDouble(s);
+            }
+            return null;
         }
-        return list.stream().mapToInt(i -> i).toArray();
     }
 
+
+    public static class TypeCaster {
+
+        public static Object castTo(Object o, Class<?> clazz) {
+            if (clazz.isArray()) {
+                return castArray(o, clazz);
+            } else {
+                return clazz.cast(o);
+            }
+        }
+
+        private static Object castArray(Object o, Class<?> clazz) {
+            Class<?> componentType = clazz.getComponentType();
+            if (componentType.isArray()) {
+                // Handle multi-dimensional arrays
+                return castMultiDimensionalArray(o, componentType);
+            } else {
+                // Handle single-dimensional arrays
+                Object[] array = (Object[]) o;
+                Object castArray = Array.newInstance(componentType, array.length);
+                for (int i = 0; i < array.length; i++) {
+                    Array.set(castArray, i, componentType.cast(array[i]));
+                }
+                return castArray;
+            }
+        }
+
+        private static Object castMultiDimensionalArray(Object o, Class<?> componentType) {
+            Class<?> componentComponentType = componentType.getComponentType();
+            if (componentComponentType.isArray()) {
+                // Recursively handle arrays
+                Object[] array = (Object[]) o;
+                Object castArray = Array.newInstance(componentType, array.length);
+                for (int i = 0; i < array.length; i++) {
+                    Array.set(castArray, i, castMultiDimensionalArray(array[i], componentComponentType));
+                }
+                return castArray;
+            } else {
+                // Handle last dimension
+                return castArray(o, componentType);
+            }
+        }
+
+    }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,28 @@
-import javafx.util.Pair;
-
 import java.util.*;
 
 class Solution {
-    public int[][] modifiedGraphEdges(int n, int[][] edges, int source, int destination, int target) {
-        return null;
+
+    public int chalkReplacer(int[] chalk, int k) {
+        long[] ps = new long[chalk.length];
+        ps[0] = chalk[0];
+        for (int i = 1; i < ps.length; i++) {
+            ps[i] = ps[i - 1] + chalk[i];
+        }
+
+        k = (int) (k % ps[ps.length - 1]);
+
+        int left = 0, right = ps.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (ps[mid] == k) {
+                return mid + 1;
+            }
+            if (ps[mid] < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
     }
 }
