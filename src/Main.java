75d92d7bfda165f74991502ee64b33ce7448ba09
Satyam Singh <satyam.singh@peoplestrong.com>
2024-09-03 22:32:31 +0530

    Input parsing for LeetCode source TC

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import java.io.FileReader;
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
         Method method = solution.getClass().getDeclaredMethods()[0];
@@ -15,7 +13,7 @@ public class Main {
                 Object[] params = new Object[method.getParameterCount()];
                 for (int i = 0; i < params.length; i++) {
                     Class<?> clazz = method.getParameterTypes()[i];
-                    params[i] = TypeCaster.castTo(parse(br.readLine(), clazz), clazz);
+                    params[i] = parse(br.readLine(), clazz);
                 }
                 System.out.println(method.invoke(solution, params));
             }
@@ -24,73 +22,68 @@ public class Main {
 
     private static Object parse(String s, Class<?> clazz) {
         if (clazz.isArray()) {
-            List<Object> list = new ArrayList<>();
-            for (String pair : s.substring(1, s.length() - 1).split(",")) {
-                list.add(parse(pair, clazz.getComponentType()));
+            Class<?> componentClazz = clazz.getComponentType();
+            List<String> parts = splitAtComma(s);
+            Object array = Array.newInstance(clazz.getComponentType(), parts.size());
+            for (int i = 0; i < parts.size(); i++) {
+                if (componentClazz.isArray()) {
+                    Array.set(array, i, parse(parts.get(i), clazz.getComponentType()));
+                } else {
+                    String part = parts.get(i);
+                    if (Objects.equals(componentClazz, int.class)) {
+                        Array.set(array, i, Integer.parseInt(part));
+                    } else if (Objects.equals(componentClazz, long.class)) {
+                        Array.set(array, i, Long.parseLong(part));
+                    } else if (Objects.equals(componentClazz, char.class)) {
+                        Array.set(array, i, part.charAt(1));
+                    } else if (Objects.equals(componentClazz, float.class)) {
+                        Array.set(array, i, Float.parseFloat(part));
+                    } else if (Objects.equals(componentClazz, boolean.class)) {
+                        Array.set(array, i, Boolean.parseBoolean(part));
+                    } else if (Objects.equals(componentClazz, double.class)) {
+                        Array.set(array, i, Double.parseDouble(part));
+                    } else if (Objects.equals(componentClazz, String.class)) {
+                        Array.set(array, i, part.substring(1, part.length() - 1));
+                    }
+                }
             }
-            return list.toArray();
-//            return Arrays.stream(s.substring(1, s.length() - 1).split(",")).map(x -> parse(x, clazz.getComponentType())).toArray();
+            return array;
         } else {
             if (Objects.equals(clazz, int.class)) {
                 return Integer.parseInt(s);
             } else if (Objects.equals(clazz, long.class)) {
                 return Long.parseLong(s);
             } else if (Objects.equals(clazz, char.class)) {
-                return s.charAt(0);
+                return s.charAt(1);
             } else if (Objects.equals(clazz, float.class)) {
                 return Float.parseFloat(s);
             } else if (Objects.equals(clazz, boolean.class)) {
                 return Boolean.parseBoolean(s);
             } else if (Objects.equals(clazz, double.class)) {
                 return Double.parseDouble(s);
+            } else if (Objects.equals(clazz, String.class)) {
+                return s.substring(1, s.length() - 1);
             }
-            return null;
         }
+        return null;
     }
 
+    private static List<String> splitAtComma(String s) {
+        if (s.charAt(1) != '[') return Arrays.asList(s.substring(1, s.length() - 1).split(","));
 
-    public static class TypeCaster {
-
-        public static Object castTo(Object o, Class<?> clazz) {
-            if (clazz.isArray()) {
-                return castArray(o, clazz);
-            } else {
-                return clazz.cast(o);
+        List<String> list = new ArrayList<>();
+        int start = 1, end = 0, count = 0;
+        while (++end < s.length()) {
+            if (s.charAt(end) == '[') {
+                count++;
+            } else if (s.charAt(end) == ']') {
+                count--;
+            }
+            if (count == 0) {
+                list.add(s.substring(start, end + 1));
+                start = ++end + 1;
             }
         }
-
-        private static Object castArray(Object o, Class<?> clazz) {
-            Class<?> componentType = clazz.getComponentType();
-            if (componentType.isArray()) {
-                // Handle multi-dimensional arrays
-                return castMultiDimensionalArray(o, componentType);
-            } else {
-                // Handle single-dimensional arrays
-                Object[] array = (Object[]) o;
-                Object castArray = Array.newInstance(componentType, array.length);
-                for (int i = 0; i < array.length; i++) {
-                    Array.set(castArray, i, componentType.cast(array[i]));
-                }
-                return castArray;
-            }
-        }
-
-        private static Object castMultiDimensionalArray(Object o, Class<?> componentType) {
-            Class<?> componentComponentType = componentType.getComponentType();
-            if (componentComponentType.isArray()) {
-                // Recursively handle arrays
-                Object[] array = (Object[]) o;
-                Object castArray = Array.newInstance(componentType, array.length);
-                for (int i = 0; i < array.length; i++) {
-                    Array.set(castArray, i, castMultiDimensionalArray(array[i], componentComponentType));
-                }
-                return castArray;
-            } else {
-                // Handle last dimension
-                return castArray(o, componentType);
-            }
-        }
-
+        return list;
     }
-
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,11 @@
 import java.util.*;
 
 class Solution {
-
-    public int chalkReplacer(int[] chalk, int k) {
-        long[] ps = new long[chalk.length];
-        ps[0] = chalk[0];
-        for (int i = 1; i < ps.length; i++) {
-            ps[i] = ps[i - 1] + chalk[i];
-        }
-
-        k = (int) (k % ps[ps.length - 1]);
-
-        int left = 0, right = ps.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (ps[mid] == k) {
-                return mid + 1;
-            }
-            if (ps[mid] < k) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return right;
+    public int test(
+            int a, char b, float c, double d, String e, boolean f,
+            int[] a1, char[] b1, float[] c1, double[] d1, String[] e1, boolean[] f1,
+            int[][] a2, char[][] b2, float[][] c2, double[][] d2, String[][] e2, boolean[][] f2
+    ) {
+        return 0;
     }
 }
