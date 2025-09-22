6d8551cfc2dfdd834e60305096b340f30afe7cae
Satyam Singh <satyam3914@gmail.com>
2025-09-23 00:34:03 +0530

    Refactoring Pair.java class to record

--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -1,16 +1,5 @@
 package main.java;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 @SuppressWarnings("unused")
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
-@ToString(includeFieldNames = false)
-public class Pair<K, V> {
-    K key;
-    V value;
+public record Pair<K, V>(K key, V value) {
 }
