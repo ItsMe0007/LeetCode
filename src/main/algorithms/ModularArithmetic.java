b1dbfe58c7f9a88f5fcff14440a0a059d336baae
Satyam Singh <satyam3914@gmail.com>
2025-09-23 09:31:47 +0530

    Added ModularArithmetic with- 1- Big multiply 2- Fast exponentiation 3- Big GCD 5- Big LCM

---
+++ src/main/algorithms/ModularArithmetic.java
@@ -0,0 +1,49 @@
+package main.algorithms;
+
+@SuppressWarnings("unused")
+public class ModularArithmetic {
+
+    private ModularArithmetic() {
+    }
+
+    public static final long DEFAULT_MOD = 1_000_000_007L;
+
+    public static long multiply(long x, long y) {
+        return multiply(x, y, DEFAULT_MOD);
+    }
+
+    public static long multiply(long x, long y, long mod) {
+        long result = 0;
+        x %= mod;
+        y %= mod;
+        while (y > 0) {
+            if ((y & 1) == 1) result = (result + x) % mod;
+            x = (x << 1) % mod;
+            y >>= 1;
+        }
+        return result;
+    }
+
+    public static long power(long base, long exp) {
+        return power(base, exp, DEFAULT_MOD);
+    }
+
+    public static long power(long base, long exp, long mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1) result = multiply(result, base, mod);
+            base = multiply(base, base, mod);
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    public static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public static long lcm(long x, long y) {
+        return (x / gcd(x, y)) * y;
+    }
+}
