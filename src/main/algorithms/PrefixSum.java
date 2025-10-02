c8cd1fe2acb23ce9f0782c073eddc7005a7fa4d0
Satyam Singh <satyam3914@gmail.com>
2025-10-02 14:54:01 +0530

    PrefixSum changed to handle mod operations

--- src/main/algorithms/PrefixSum.java
+++ src/main/algorithms/PrefixSum.java
@@ -1,37 +1,61 @@
 package main.algorithms;
 
 @SuppressWarnings("unused")
-public class PrefixSum {
+public abstract class PrefixSum {
+    protected final long[] ps;
+    public static final long DEFAULT_MOD = 1_000_000_007L;
 
-    private final long[] preSum;
-
-    public PrefixSum(int[] a) {
-        int n = a.length;
-        preSum = new long[n + 1];
-        for (int i = 0; i < n; i++) preSum[i + 1] = preSum[i] + a[i];
+    private PrefixSum(long[] ps) {
+        this.ps = ps;
     }
 
-    public long rangeSumInclusive(int l, int r) {
-        return preSum[r + 1] - preSum[l];
+    public static PrefixSum of(int[] arr) {
+        return new PlainPrefixSum(preparePS(arr));
     }
 
-    public static class PrefixSum2D {
-        private final long[][] preSum2D;
+    public static PrefixSum ofMod(int[] arr) {
+        return ofMod(arr, DEFAULT_MOD);
+    }
 
-        public PrefixSum2D(int[][] a) {
-            int m = a.length;
-            int n = a[0].length;
-            preSum2D = new long[m + 1][n + 1];
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    preSum2D[i + 1][j + 1] = preSum2D[i + 1][j] + preSum2D[i][j + 1] - preSum2D[i][j] + a[i][j];
-                }
-            }
+    public static PrefixSum ofMod(int[] arr, long mod) {
+        return new ModPrefixSum(preparePS(arr), mod);
+    }
+
+    public abstract long rangeSumInclusive(int left, int right);
+
+    private static long[] preparePS(int[] arr) {
+        int n = arr.length;
+        long[] ps = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            ps[i + 1] = ps[i] + arr[i];
+        }
+        return ps;
+    }
+
+    private static final class PlainPrefixSum extends PrefixSum {
+        private PlainPrefixSum(long[] ps) {
+            super(ps);
         }
 
-        public long rangeSumInclusive(int i1, int j1, int i2, int j2) {
-            return preSum2D[i2 + 1][j2 + 1] - preSum2D[i1][j2 + 1] - preSum2D[i2 + 1][j1] + preSum2D[i1][j1];
+        @Override
+        public long rangeSumInclusive(int left, int right) {
+            return ps[right + 1] - ps[left];
+        }
+    }
+
+    private static final class ModPrefixSum extends PrefixSum {
+        private final long mod;
+
+        private ModPrefixSum(long[] ps, long mod) {
+            super(ps);
+            this.mod = mod;
         }
 
+        @Override
+        public long rangeSumInclusive(int left, int right) {
+            long res = (ps[right + 1] - ps[left]) % mod;
+            if (res < 0) res += mod;
+            return res;
+        }
     }
 }
