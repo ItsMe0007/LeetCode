bf15e0d587a38d128ff1ae301a351bf027478260
Satyam Singh <satyam3914@gmail.com>
2025-12-19 03:07:23 +0530

    Added RangeDiff2D class with inclusive range update and toArray functionality

---
+++ src/main/algorithms/RangeDiff2D.java
@@ -0,0 +1,47 @@
+package main.algorithms;
+
+@SuppressWarnings("unused")
+public class RangeDiff2D {
+
+    public final int minRow, minCol;
+    private final int rowSize, colSize;
+    private final int[][] diff;
+
+    public RangeDiff2D(int minRow, int maxRow, int minCol, int maxCol) {
+        this.minRow = minRow;
+        this.minCol = minCol;
+        this.rowSize = maxRow - minRow + 1;
+        this.colSize = maxCol - minCol + 1;
+        this.diff = new int[rowSize + 1][colSize + 1];
+    }
+
+    public RangeDiff2D(int rowSize, int colSize) {
+        this(0, rowSize - 1, 0, colSize - 1);
+    }
+
+    public void addRangeInclusive(int row1, int col1, int row2, int col2, int delta) {
+        row1 -= minRow;
+        col1 -= minCol;
+        row2 -= minRow;
+        col2 -= minCol;
+        diff[row1][col1] += delta;
+        diff[row2 + 1][col1] -= delta;
+        diff[row1][col2 + 1] -= delta;
+        diff[row2 + 1][col2 + 1] += delta;
+    }
+
+    public int[][] toArray() {
+        int[][] res = new int[rowSize][colSize];
+
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                int val = diff[i][j];
+                if (i > 0) val += res[i - 1][j];
+                if (j > 0) val += res[i][j - 1];
+                if (i > 0 && j > 0) val -= res[i - 1][j - 1];
+                res[i][j] = val;
+            }
+        }
+        return res;
+    }
+}
