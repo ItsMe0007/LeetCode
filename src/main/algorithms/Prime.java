3a7d65d4033bb50abda08758730baf204fc27246
Satyam Singh <satyam3914@gmail.com>
2026-06-27 22:22:06 +0530

    Prime: Changed implementation with SPF array

--- src/main/algorithms/Prime.java
+++ src/main/algorithms/Prime.java
@@ -2,53 +2,57 @@ package main.algorithms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class Prime {
     private static final int DEFAULT_MAX_N = 1_000_000;
 
-    public final boolean[] isPrime;
-    public final List<Integer> primes;
+    public final int[] spf;
+    public final int[] primes;
 
     public Prime() {
         this(DEFAULT_MAX_N);
     }
 
     public Prime(int inclusiveLimit) {
-        isPrime = new boolean[inclusiveLimit + 1];
-        primes = new ArrayList<>();
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
+        spf = new int[inclusiveLimit + 1];
+        int[] tempPrimes = new int[Math.min((inclusiveLimit + 1) >> 1, 78_500)];
+        int size = 0;
+        spf[0] = spf[1] = -1;
 
-        for (int m = 4; m <= inclusiveLimit; m += 2) isPrime[m] = false;
+        for (int num = 2; num <= inclusiveLimit; num++) {
+            if (spf[num] == 0) {
+                spf[num] = num;
+                tempPrimes[size++] = num;
+            }
 
-        for (int p = 3; p * p <= inclusiveLimit; p += 2) {
-            if (isPrime[p]) {
-                for (int m = p * p; m <= inclusiveLimit; m += (p << 1)) {
-                    isPrime[m] = false;
-                }
+            for (int i = 0; i < size; i++) {
+                int prime = tempPrimes[i];
+                if (prime > spf[num] || (long) prime * num > inclusiveLimit) break;
+                spf[prime * num] = prime;
             }
         }
-        if (inclusiveLimit >= 2) primes.add(2);
-        for (int p = 3; p <= inclusiveLimit; p += 2) {
-            if (isPrime[p]) primes.add(p);
-        }
+        primes = Arrays.copyOf(tempPrimes, size);
+    }
+
+    public boolean isPrime(int num) {
+        return spf[num] == num;
     }
 
     private static final int[] SMALL = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 
     public static boolean checkIfPrime(int num) {
         if (num < 2) return false;
-        if ((num & 1L) == 0) return num == 2;
+        if ((num & 1) == 0) return num == 2;
 
-        for (int p : SMALL) {
-            if (num == p) return true;
-            if (num % p == 0) return false;
+        for (int prime : SMALL) {
+            if (num == prime) return true;
+            if (num % prime == 0) return false;
         }
 
-        int factor = 11;
+        int factor = 41;
+
         while (factor <= num / factor) {
             if (num % factor == 0 || num % (factor + 2) == 0) return false;
             factor += 6;
@@ -56,21 +60,25 @@ public class Prime {
         return true;
     }
 
-    public List<Integer> getPrimeFactors(int n) {
-        if (n <= 1) return Collections.emptyList();
-        if (n < isPrime.length && isPrime[n]) return Collections.singletonList(n);
+    public List<Integer> getPrimeFactors(int num) {
         List<Integer> factors = new ArrayList<>();
-
-        for (int p : primes) {
-            if ((long) p * p > n) break;
-            while (n % p == 0) {
-                factors.add(p);
-                n /= p;
-            }
-            if (n < isPrime.length && isPrime[n]) break;
+        while (num > 1) {
+            int prime = spf[num];
+            factors.add(prime);
+            num /= prime;
         }
-        if (n > 1) factors.add(n);
         return factors;
     }
 
+    public List<Integer> getDistinctPrimeFactors(int num) {
+        List<Integer> factors = new ArrayList<>();
+        while (num > 1) {
+            int prime = spf[num];
+            factors.add(prime);
+            do {
+                num /= prime;
+            } while (num > 1 && spf[num] == prime);
+        }
+        return factors;
+    }
 }
